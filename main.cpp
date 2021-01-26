9807b135f303ac1b36ec9a11517f5e7ea5808387
Lx <478684715@qq.com>
2021-01-26 12:37:13 +0800

    循环队列的实现

---
+++ main.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+using namespace std;
+
+//循环队列
+class Queue
+{
+public:
+	Queue(int size = 20)
+	{
+		_pQue = new int[size];
+		_front = _rear = 0;
+		_size = size;
+	}
+	Queue(const Queue& src)
+	{
+		_size = src._size;
+		_front = src._front;
+		_rear = src._rear;
+		_pQue = new int[_size];
+		for (int i = _front; i != _rear; i = (i + 1) % _size)
+		{
+			_pQue[i] = src._pQue[i];
+		}
+	}
+	Queue& operator=(const Queue& src)
+	{
+		if (this == &src)
+		{
+			return *this;
+		}
+		delete[]_pQue;
+		_size = src._size;
+		_front = src._front;
+		_rear = src._rear;
+		_pQue = new int[_size];
+		for (int i = _front; i != _rear; i = (i + 1) % _size)
+		{
+			_pQue[i] = src._pQue[i];
+		}
+	}
+	~Queue()
+	{
+		delete[]_pQue;
+		_pQue = nullptr;
+	}
+	void addQue(int val)//入队
+	{
+		if (full())
+		{
+			resize();
+		}
+		_pQue[_rear] = val;
+		_rear = (_rear + 1) % _size;
+	}
+	void pop()//出队
+	{
+		if (empty())
+		{
+			return;
+		}
+		_front = (_front + 1) % _size;
+	}
+	int top()//获取队头元素
+	{
+		return _pQue[_front];
+	}
+	bool full()
+	{
+		return(_rear + 1) % _size == _front;
+	}
+	bool empty()
+	{
+		return _front == _rear;
+	}
+
+
+private:
+	int* _pQue;//申请队列的数组空间
+	int _front;//指示队头的位置
+	int _rear;//指示队尾的位置
+	int _size;//队列扩容的总大小
+
+	void resize()
+	{
+		int* ptmp = new int[_size * 2];
+		int index = 0;
+		for (int i = _front; i != _rear; i = (i + 1) % _size)
+		{
+			ptmp[index++] = _pQue[i];
+		}
+		delete[]_pQue;
+		_pQue = ptmp;
+		_front = 0;
+		_rear = index;
+		_size *= 2;
+	}
+};
+
+int main()
+{
+	Queue queue;
+	for (int i = 0; i < 20; ++i)
+	{
+		queue.addQue(rand() % 100);
+	}
+	while (!queue.empty())
+	{
+		cout << queue.top() << " ";
+		queue.pop();
+	}
+	cout << endl;
+	Queue queue1 = queue;
+	queue1 = queue;
+	
+	return 0;
+}
